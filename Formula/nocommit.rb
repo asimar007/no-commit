b951a83a10d11ebdbaf2c4c070f5b40440aa224f
Asim <blackhatcode786@gmail.com>
2026-07-13 10:21:17 +0530

    chore: update nocommit to 1.0.

--- Formula/nocommit.rb
+++ Formula/nocommit.rb
@@ -3,8 +3,8 @@ require "language/node"
 class Nocommit < Formula
   desc "AI-powered CLI that writes your git commit messages using Google Gemini"
   homepage "https://github.com/asimar007/no-commit"
-  url "https://registry.npmjs.org/nocommit/-/nocommit-1.0.0.tgz"
-  sha256 "d154ee8cbe8e40d87dd064f13c62a5fb80cd0988547571f651456304c3f52b80"
+  url "https://registry.npmjs.org/nocommit/-/nocommit-1.0.1.tgz"
+  sha256 "e58a465bd3ab86e7d7a81012d20238380f7c4ccfac3bdc40b93877dcb97becf7"
   license "MIT"
 
   depends_on "node"
